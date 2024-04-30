20aabdd8ef6d21bfd17b002da72daad0401d9ec4
Paulina <mariapaulinagarciamarin@gmail.com>
2024-04-30 18:36:16 -0500

    Added

--- ejercicio2.js
+++ ejercicio2.js
@@ -2,8 +2,12 @@ let numero1 = parseFloat(prompt("Ingrese un número:"));
 let numero2 = parseFloat(prompt("Ingrese otro número:"));
 let operacion = prompt("Ingrese una operación (Suma, resta, multiplicación, división):");
 
-if (operacion == "+") {
+if (operacion ===  "+") {
     document.write(numero1 + numero2);
-} else if (operacion == "-") {
+} else if (operacion === "-") {
     document.write(numero1 - numero2);
+} else if (operacion === "*") {
+    document.write(numero1 * numero2);
+} else if (operacion === "/") {
+    document.write(numero1 / numero2);
 }
